73ee28ae1548341ed322d8df6a58c2ac1e09d9d8
Avico-Dazs <takahashi-yuusuke@koyoele.co.jp>
2021-10-15 09:48:15 +0900

    プロジェクト ファイルを追加します。

---
+++ ResultPractice/Result3.h
@@ -0,0 +1,112 @@
+#pragma once
+
+template <typename T, typename E>
+struct Result {
+
+    Result(T const& ok) : t(tag::OK) {
+        ok_ = ok;
+    }
+
+    Result(E const& e) : t(tag::ERROR) {
+        err_ = e;
+    }
+
+    ~Result() {
+        switch (t) {
+        case tag::OK:
+            ok_.~T();
+            break;
+        case tag::ERROR:
+            err_.~E();
+            break;
+        }
+    }
+
+    Result(Result const& r) : t(r.t) {
+        switch (t) {
+        case tag::OK:
+            ok_ = r.ok_;
+            break;
+        case tag::ERROR:
+            err_ = r.err_;
+            break;
+        }
+    }
+
+    bool Valid() const {
+        return (t == tag::OK);
+    }
+
+    T const& Get() const {
+        if (t != tag::OK) {
+            throw "invalid get operation";
+        }
+        return ok_;
+    }
+
+    E const& GetError() const {
+        if (t != tag::ERROR) {
+            throw "invalid get error operation";
+        }
+        return err_;
+    }
+
+private:
+    enum class tag {
+        OK,
+        ERROR,
+    };
+    tag t;
+    union {
+        T ok_;
+        E err_;
+    };
+};
+
+template <typename T>
+struct ok_value {
+    explicit ok_value(T t) : t(t) {}
+
+    template <typename V, typename E>
+    operator Result<V, E>() const {
+        return Result<V, E>(t);
+    }
+
+private:
+    T t;
+};
+
+//template <typename T>
+//template <typename V, typename E>
+//ok_value<T>::operator Result<V, E>() const {
+//    return Result<V, E>(t);
+//}
+
+template <typename T>
+ok_value<T> Ok(T t) {
+    return ok_value<T>(t);
+}
+
+template <typename T>
+struct error_value {
+    explicit error_value(T t) : t(t) {}
+
+    template <typename V, typename E>
+    operator Result<V, E>() const {
+        return Result<V, E>(t);
+    }
+
+private:
+    T t;
+};
+
+//template <typename T>
+//template <typename V, typename E>
+//error_value<T>::operator Result<V, E>() const {
+//    return Result<V, E>(t);
+//}
+
+template <typename T>
+error_value<T> Error(T t) {
+    return error_value<T>(t);
+}
---
+++ ResultPractice/ResultPractice.cpp
@@ -0,0 +1,61 @@
+﻿// ResultPractice.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include "Result3.h"
+
+Result<int, std::string> ParseDigit(char c) {
+    if (c < '0' || '9' < c) {
+        return Error("invalid character");
+    }
+
+    return Ok(c - '0');
+}
+
+int main()
+{
+    Result<int, std::string> result_ok = ParseDigit('4');
+    if (result_ok.Valid())
+    {
+        int num = result_ok.Get();
+        // 4
+    }
+
+    Result<int, std::string> result_err = ParseDigit('g');
+    if (!result_err.Valid())
+    {
+        std::string num = result_err.GetError();
+        // "invalid character"
+    }
+
+
+
+
+
+
+
+
+
+
+
+    std::cout << "Hello World!\n";
+    auto res = ParseDigit('4');
+    std::cout << (res.Valid() ? "Valid" : "Invalid") << std::endl;
+    std::cout << res.Get() << std::endl;
+    auto res2 = ParseDigit('d');
+    std::cout << (res2.Valid() ? "Valid" : "Invalid") << std::endl;
+    std::cout << res2.GetError() << std::endl;
+
+
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
